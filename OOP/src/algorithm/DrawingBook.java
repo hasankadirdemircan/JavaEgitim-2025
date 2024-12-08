a3516ae7aed6375348cf96a0b934e693169d98b5
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2024-12-08 14:05:56 +0300

    DrawingBook algorithm

---
+++ OOP/src/algorithm/DrawingBook.java
@@ -0,0 +1,14 @@
+package algorithm;
+
+public class DrawingBook {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int pageCount(int n, int p) {
+
+        return 0;
+    }
+
+}
