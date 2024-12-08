d1a3afbfef70fd6be07614635fcadae95c90b290
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2024-12-08 10:19:09 +0300

    angry professor

--- OOP/src/algorithm/AngryProfessor.java
+++ OOP/src/algorithm/AngryProfessor.java
@@ -1,4 +1,28 @@
 package algorithm;
 
 public class AngryProfessor {
+
+    public static void main(String[] args) {
+        int[] a = {-2, -1, 0, 1, 2};
+        int k = 3;
+        String decisionOfProfessor = angryProfessor(k, a);
+        System.out.println("will the class cancel : " + decisionOfProfessor);
+    }
+
+
+    public static String angryProfessor(int k, int[] a) {
+        int studentCountInClass = 0;
+
+        for(int studentArriveTime : a) {
+            if(studentArriveTime <= 0) {
+                studentCountInClass++;
+            }
+        }
+
+        if(studentCountInClass >= k) {
+            return "NO"; // the class is cancelled?
+        }
+
+        return "YES";
+    }
 }
