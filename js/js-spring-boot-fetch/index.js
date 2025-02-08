9fa66aca1c7ffa00a773503820cf56f6a2bb98b9
Hasan Kadir Demircan <hkdemircan06@gmail.com>
2025-02-08 12:34:39 +0300

    added CRUD on javascript

--- js/js-spring-boot-fetch/index.js
+++ js/js-spring-boot-fetch/index.js
@@ -1,3 +1,5 @@
+document.getElementById('updateButton').style.visibility = 'hidden';
+
 fetch('http://127.0.0.1:8085/api/users', {
     method: 'GET'
 }).then((response) => response.json())
@@ -22,7 +24,78 @@ function updateTable(data) {
             <td>${element.firstName}</td>
             <td>${element.lastName}</td>
             <td>${element.salary}</td>
-        `;
+            <td><button class="btn btn-danger" onclick="deleteUser(${element.id})"> Delete </button>
+            <td><button class="btn btn-warning" onclick="updateUser(${element.id})"> Update </button>
+            `;
         tableBody.appendChild(row);
     });
 }
+
+function updateUser(id) {
+    document.getElementById('saveButton').style.visibility = 'hidden';
+    document.getElementById('updateButton').style.visibility = 'visible';
+
+    console.log("updateUser", id)
+    fetch('http://127.0.0.1:8085/api/users/'+id, {
+        method: 'GET'
+    }).then((response) => response.json())
+        .then((user) => {
+            console.log("user");
+            console.log(user);
+            document.getElementById('firstName').value = user.firstName
+            document.getElementById('lastName').value = user.lastName
+            document.getElementById('salary').value = user.salary
+            document.getElementById('city').value = user.city
+            document.getElementById('age').value = user.age
+            document.getElementById('userId').value = user.id
+        })
+    
+}
+
+
+function deleteUser(id) {
+    console.log("user id : ", id)
+    fetch('http://127.0.0.1:8085/api/users/'+id, {
+        method: 'DELETE'
+    }).then((data) => {
+            console.log("delete success")
+            window.location.reload();
+        })
+}
+
+
+document.querySelector("#createUser").addEventListener("submit", function(e){
+        const form = document.getElementById('createUser');
+       
+        e.preventDefault();    //stop form from submitting
+        console.log("submit function ")
+        const firstName = document.getElementById('firstName').value
+        const lastName = document.getElementById('lastName').value
+        const salary = document.getElementById('salary').value
+        const city = document.getElementById('city').value
+        const age = document.getElementById('age').value
+        const id = document.getElementById('userId').value
+        console.log("firstName: ", firstName)
+        console.log("lastName: ", lastName)
+        console.log("salary: ", salary)
+        console.log("city: ", city)
+        console.log("age: ", age)
+        console.log("id : ", id)
+        form.reset();
+
+        fetch('http://127.0.0.1:8085/api/users/create', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify({firstName: firstName, lastName: lastName,
+                salary: salary, city: city, age: age, id: id
+            })
+        }).then((response) => response.json())
+        .then((data) => {
+            console.log("data");
+            console.log(data);
+            window.location.reload();
+        })
+});
